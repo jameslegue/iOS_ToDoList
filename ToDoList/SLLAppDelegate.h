697ee33e2d075a96c6bf04ef69f47717521a3f90
James Legue <james.legue@gmail.com>
2014-02-06 20:28:48 -0500

    Initial Commit

---
+++ ToDoList/SLLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLLAppDelegate.h
+//  ToDoList
+//
+//  Created by James Legue on 2/6/2014.
+//  Copyright (c) 2014 StartLight Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by James Legue on 2/6/2014.
+//  Copyright (c) 2014 StartLight Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLLAppDelegate class]));
+    }
+}
